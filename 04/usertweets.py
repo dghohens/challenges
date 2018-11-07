c51c114804107d50babab31072f10a0a1696c179
Donald Hohenstein <dghohens@gmail.com>
2018-11-07 08:11:28 -0500

    Day 7 of 100 days of code. PyBites challenge 4 in progress.

--- 04/usertweets.py
+++ 04/usertweets.py
@@ -11,15 +11,60 @@ DEST_DIR = 'data'
 EXT = 'csv'
 NUM_TWEETS = 100
 
+Tweet = namedtuple('Tweet', 'id_str created_at text')
+
 
 class UserTweets(object):
-    """TODOs:
-    - create a tweepy api interface
-    - get all tweets for passed in handle
-    - optionally get up until 'max_id' tweet id
-    - save tweets to csv file in data/ subdirectory
-    - implement len() an getitem() magic (dunder) methods"""
-    pass
+
+    def __init__(self, handle, min_id=None):
+        """Get handle and optional max_id.
+        Use tweepy.OAuthHandler, set_access_token and tweepy.API
+        to create api interface.
+        Use _get_tweets() helper to get a list of tweets.
+        Save the tweets as data/<handle>.csv"""
+
+        # This part is from the tweepy OAuth tutorial
+        auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+        try:
+            redirect_url = auth.get_authorization_url()
+        except tweepy.TweepError:
+            print('Error! Failed to get request token')
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+        self.api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+        self._tweets = list(self._get_tweets())
+        self._save_tweets()
+
+    def _get_tweets(self):
+        """Hint: use the user_timeline() method on the api you defined in init.
+        See tweepy API reference: http://docs.tweepy.org/en/v3.5.0/api.html
+        Use a list comprehension / generator to filter out fields
+        id_str created_at text (optionally use namedtuple)"""
+        tweet_tuple = namedtuple('Tweet', ['id_str','created_at','text'])
+        json_tweets = self.api.user_timeline(handle, min_id=None)
+
+        user_tweets=[]
+        a = 1
+
+        for i in json_tweets:
+
+
+        return user_tweets
+
+    def _save_tweets(self):
+        """Use the csv module (csv.writer) to write out the tweets.
+        If you use a namedtuple get the column names with Tweet._fields.
+        Otherwise define them as: id_str created_at text
+        You can use writerow for the header, writerows for the rows"""
+        pass
+
+    def __len__(self):
+        """See http://pybit.es/python-data-model.html"""
+        pass
+
+    def __getitem__(self, pos):
+        """See http://pybit.es/python-data-model.html"""
+        pass
 
 
 if __name__ == "__main__":
