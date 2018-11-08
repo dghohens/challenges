4b25deb2c6ef0b4b9c3ba9c380210cb2bcce819a
Donald Hohenstein <dghohens@gmail.com>
2018-11-08 08:00:24 -0500

    Day 8 of 100 days of code. PyBites challenge 4 almost complete.

--- 04/usertweets.py
+++ 04/usertweets.py
@@ -11,12 +11,13 @@ DEST_DIR = 'data'
 EXT = 'csv'
 NUM_TWEETS = 100
 
+
 Tweet = namedtuple('Tweet', 'id_str created_at text')
 
 
 class UserTweets(object):
 
-    def __init__(self, handle, min_id=None):
+    def __init__(self, handle, max_id=None):
         """Get handle and optional max_id.
         Use tweepy.OAuthHandler, set_access_token and tweepy.API
         to create api interface.
@@ -41,30 +42,31 @@ class UserTweets(object):
         Use a list comprehension / generator to filter out fields
         id_str created_at text (optionally use namedtuple)"""
         tweet_tuple = namedtuple('Tweet', ['id_str','created_at','text'])
-        json_tweets = self.api.user_timeline(handle, min_id=None)
-
-        user_tweets=[]
-        a = 1
-
+        json_tweets = self.api.user_timeline(handle, count = NUM_TWEETS)
+        list_of_tweets = []
         for i in json_tweets:
-
-
-        return user_tweets
+            list_of_tweets.append(tweet_tuple(i['id_str'], i['created_at'], i['text']))
+        return list_of_tweets
 
     def _save_tweets(self):
         """Use the csv module (csv.writer) to write out the tweets.
         If you use a namedtuple get the column names with Tweet._fields.
         Otherwise define them as: id_str created_at text
         You can use writerow for the header, writerows for the rows"""
+        with open('data\\some_handle.csv', 'w+', encoding = 'utf-8', newline = '') as csvfile:
+            tweetwriter = csv.writer(csvfile)
+            tweetwriter.writerow(['id_str'] + ['created_at'] + ['text'])
+            for i in self._get_tweets():
+                tweetwriter.writerow(i)
         pass
 
     def __len__(self):
         """See http://pybit.es/python-data-model.html"""
-        pass
+        return len(self._tweets)
 
     def __getitem__(self, pos):
         """See http://pybit.es/python-data-model.html"""
-        pass
+        return self._tweets[pos]
 
 
 if __name__ == "__main__":
