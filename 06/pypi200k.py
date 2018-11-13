27f563e62a1d57ff817c5b4b1d0fe71254c0ec19
Donald Hohenstein <dghohens@gmail.com>
2018-11-13 07:57:33 -0500

    Day 13 of 100 days of code, PyBites challenge 6 in progress.

--- 06/pypi200k.py
+++ 06/pypi200k.py
@@ -1,7 +1,14 @@
 import urllib.request
+import xmlrpc.client
+import arrow
 
 NUM_PACKS_TO_REACH = 100000
 PYPI = 'https://pypi.python.org/pypi'
+
+def get_packages(timestamp):
+    client = xmlrpc.client.ServerProxy('https://pypi.org/pypi')
+    changes = client.changelog(timestamp)
+    return changes
 
 def package_list():
     pack_list = urllib.request.urlopen('https://www.pypi.org/simple/')
@@ -10,4 +17,22 @@ def package_list():
 if __name__ == "__main__":
     print('PyPI hit 100,000 packages on March 4th, 2017! https://twitter.com/pybites/status/838178449999081472')
     print("Here's when we estimate PyPI will hit 200,000 packages:")
-    print(package_list())
+    current_time = arrow.utcnow()
+    print(current_time)
+    print(current_time.timestamp)
+    print(len(get_packages(current_time.timestamp)))
+    for i in range(24):
+        if current_time.month == 1:
+            current_time = current_time.replace(years=-1)
+            current_time = current_time.replace(month = 12)
+            # call package list
+            #print(current_time)
+            print(current_time.date)
+            print(len(get_packages(current_time.timestamp)))
+        else:
+            current_time = current_time.replace(month = (current_time.month - 1))
+            #call package list
+            #print(current_time)
+            print(current_time.date)
+            print(len(get_packages(current_time.timestamp)))
+#    print(package_list())
