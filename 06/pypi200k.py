bf0b454295b76bb24a763520fc0bf4d926a4ea45
Donald Hohenstein <dghohens@gmail.com>
2018-11-14 08:03:22 -0500

    Day 14 of 100 days of code, PyBites challenge 6 in progress.

--- 06/pypi200k.py
+++ 06/pypi200k.py
@@ -1,5 +1,6 @@
 import urllib.request
 import xmlrpc.client
+import re
 import arrow
 
 NUM_PACKS_TO_REACH = 100000
@@ -11,13 +12,35 @@ def get_packages(timestamp):
     return changes
 
 def package_list():
-    pack_list = urllib.request.urlopen('https://www.pypi.org/simple/')
-    return pack_list.read()
+    ''' This function pulls all packages from pypi.org/simple and returns a list of the package names.
+    I'm only going to check 1/100 of the packages to save memory and API calls.'''
+    pack_regex = re.compile(b'">(\S+)</a>')
+    pack_list1 = urllib.request.urlopen('https://www.pypi.org/simple/')
+    pack_list2 = pack_list1.read()
+    pack_list3 = pack_list2.split(b'\n')
+    pack_list4 = []
+
+    for i in pack_list3[6:-2:100]:
+        a = pack_regex.search(i)
+        pack_list4.append(a.group(1).decode('utf-8'))
+
+    return pack_list4
+
+def get_dates(packagelist):
+    '''Check dates from pypi.org/pypi/<package>/json, strip out dates, find first date. Return list of all first upload dates.'''
+    first_dates = []
+    date_regex = re.compile(b'_time":"(\d\d\d\d-\d\d-\d\d)T\d\d')
+    for i in packagelist:
+        a = []
+
 
 if __name__ == "__main__":
     print('PyPI hit 100,000 packages on March 4th, 2017! https://twitter.com/pybites/status/838178449999081472')
     print("Here's when we estimate PyPI will hit 200,000 packages:")
-    current_time = arrow.utcnow()
+
+
+
+    '''    current_time = arrow.utcnow()
     print(current_time)
     print(current_time.timestamp)
     print(len(get_packages(current_time.timestamp)))
@@ -35,4 +58,7 @@ if __name__ == "__main__":
             #print(current_time)
             print(current_time.date)
             print(len(get_packages(current_time.timestamp)))
-#    print(package_list())
+    '''
+    packlist = package_list()
+    print(packlist)
+    print(len(packlist))
